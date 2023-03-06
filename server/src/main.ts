6f011825ab722888bd9c920d1f0743c1c34db2f4
Szymon Burak <sajmon0031@gmail.com>
2023-03-06 21:03:34 +0100

    Fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '250mb' }));
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
   });
   await app.listen(5000);
